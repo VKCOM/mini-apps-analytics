d769206ec68891f6b1f5135f38ceb6889d975f8f
Aleksey Chepurko <a.chepurko@vk.team>
2023-11-08 11:11:10 +0300

    feat(react) useLaunchAnalytics: добавил проверку на isAppReady

--- packages/react/src/useLaunchAnalytics/useLaunchAnalytics.ts
+++ packages/react/src/useLaunchAnalytics/useLaunchAnalytics.ts
@@ -8,16 +8,19 @@ import { analyticsContext } from '../context';
  * регистрирует analyticsContext -> launchEventService.registerScreenListener при старте приложения
  *
  */
-export const useLaunchAnalytics = () => {
+export const useLaunchAnalytics = (isAppReady = true) => {
   const { launchEventService } = useContext(analyticsContext);
 
   /** Регистрируем слушателя открытия первой страницы на старте */
   useLayoutEffect(() => {
+    if (!isAppReady) {
+      return;
+    }
     const launchEventInterval = launchEventService.registerScreenListener();
 
     return () => {
       /** Очищаем интервал, если страница не была до конца загружена, но пользователь уже перешел на другую */
       clearInterval(launchEventInterval);
     };
-  }, [launchEventService]);
+  }, [launchEventService, isAppReady]);
 };
