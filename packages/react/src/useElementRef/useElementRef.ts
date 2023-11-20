f774c3fa9d44f4c73162b25e17b351c8aff884e9
Aleksey Chepurko <a.chepurko@vk.team>
2023-11-20 14:06:49 +0300

    fix(react) перенес вызов сбора информации внутрь таймаута

--- packages/react/src/useElementRef/useElementRef.ts
+++ packages/react/src/useElementRef/useElementRef.ts
@@ -48,18 +48,18 @@ export const useItemRef = <T extends HTMLElement>(id: ID): MutableRefObject<T | 
 
   /** Собираем данные на основе дата-аттрибутов с элемента внутри блока */
   useEffect(() => {
-    if (!itemRef.current) {
-      return;
-    }
-
-    const itemInfo = getItemInfo(null, itemRef.current);
-
     /**
      * При обновлении страницы всплывают от чайлда к родителю, что приводит к тому, что, на момент
      * регистрации айтемов, соответствующий блок еще не зарегистрирован. Чтобы обойти ограничение реакта
      * смещаем вызов колбека на след тик
      */
     setTimeout(() => {
+      if (!itemRef.current) {
+        return;
+      }
+
+      const itemInfo = getItemInfo(null, itemRef.current);
+
       CurrentStateStorage.addItemByBlockId(id.toString(), {
         id: itemInfo.actionElementId,
         name: itemInfo.actionElementName,
